238644c627b505cb8f181a4c61994e2900a7bf2e
Student <example@example.com>
2018-11-27 16:20:06 -0800

    refactor for redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,17 +8,9 @@ import Moment from 'moment';
 import Admin from './Admin';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import c from './../constants';
 
 class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    console.log(props)
-    this.state = {
-      selectedTicket: null
-    };
-    this.handleChangingSelectedTicket = this.handleChangingSelectedTicket.bind(this);
-  }
 
   componentDidMount() {
     this.waitTimeUpdateTimer = setInterval(() =>
@@ -31,16 +23,18 @@ class App extends React.Component {
     clearInterval(this.waitTimeUpdateTimer);
   }
 
-  // updateTicketElapsedWaitTime() {
-  //   var newMasterTicketList = Object.assign({}, this.state.masterTicketList);
-  //   Object.keys(newMasterTicketList).forEach(ticketId => {
-  //     newMasterTicketList[ticketId].formattedWaitTime = (newMasterTicketList[ticketId].timeOpen).fromNow(true);
-  //   });
-  //   this.setState({masterTicketList: newMasterTicketList});
-  // }
-
-  handleChangingSelectedTicket(ticketId){
-    this.setState({selectedTicket: ticketId});
+  updateTicketElapsedWaitTime() {
+    const { dispatch } = this.props;
+    Object.keys(this.props.masterTicketList).map(ticketId => {
+      const ticket = this.props.masterTicketList[ticketId];
+      const newFormattedWaitTime = ticket.timeOpen.fromNow(true);
+      const action = {
+        type: c.UPDATE_TIME,
+        id: ticketId,
+        formattedWaitTime: newFormattedWaitTime
+      };
+      dispatch(action);
+    });
   }
 
   render(){
@@ -50,9 +44,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' render={()=><TicketList ticketList={this.props.masterTicketList} />} />
           <Route path='/newticket' render={()=><NewTicketControl/>} />
-          <Route path='/admin' render={(props)=><Admin ticketList={this.props.masterTicketList} currentRouterPath={props.location.pathname}
-            onTicketSelection={this.handleChangingSelectedTicket}
-            selectedTicket={this.state.selectedTicket}/>} />
+          <Route path='/admin' render={(props)=><Admin ticketList={this.props.masterTicketList} currentRouterPath={props.location.pathname}/>} />
           <Route component={Error404} />
         </Switch>
       </div>
@@ -67,8 +59,8 @@ App.propTypes = {
 
 const mapStateToProps = state => {
   return {
-    masterTicketList: state
-  }
-}
+    masterTicketList: state.masterTicketList
+  };
+};
 
 export default withRouter(connect(mapStateToProps)(App));
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import c from './../constants';
 
 function Ticket(props){
+
+  function handleSavingSelectedTicket(ticketId){
+      const { dispatch } = props;
+      const action = {
+        type: c.SELECT_TICKET,
+        ticketId: ticketId
+      };
+      dispatch(action);
+    }
+
   const ticketInformation =
     <div>
       <h3>{props.location} - {props.names}</h3>
-      <h4>{props.formattedWaitTime}</h4>
+      <p>Submitted {props.formattedWaitTime} ago</p>
+      <p>{props.issue}</p>
       <hr/>
     </div>;
   if (props.currentRouterPath === '/admin'){
     return (
-      <div onClick={() => {props.onTicketSelection(props.ticketId);}}>
+      <div onClick={() => {handleSavingSelectedTicket(props.ticketId);}}>
         {ticketInformation}
       </div>
     );
@@ -27,10 +40,9 @@ Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   issue: PropTypes.string,
-  formattedWaitTime: PropTypes.string.isRequired,
+  // formattedWaitTime: PropTypes.string.isRequired,
   currentRouterPath: PropTypes.string,
-  onTicketSelection: PropTypes.func,
   ticketId: PropTypes.string.isRequired
 };
 
-export default Ticket;
+export default connect()(Ticket);
